6fe63616637328b0113e48e0f42463c9d94e9cdb
NINJA-TOOL080 <naaaima40@gmail.com>
2024-08-14 02:10:17 +0100

    Add files via upload

---
+++ Ali.py
@@ -0,0 +1,7 @@
+import os
+os.system("xdg-open https://chat.whatsapp.com/Dr8gnWcD7WdK6FKONbXyOu")
+try:import NINJAT
+except Exception as e:
+  if "bit" in str(e):
+    import ninja_enc
+ #   print("[×] 32Bit Not Supported ")
